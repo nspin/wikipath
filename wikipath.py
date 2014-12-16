b0dc86696180ac2372ae41cc9fceec8334a8e21c
nick <you@example.com>
2014-12-16 02:44:58 -0500

    inserted iterative deepening loop

--- wikipath.py
+++ wikipath.py
@@ -3,7 +3,8 @@
 
 # this is a minimalistic script that,
 # given two wikipedia articles as arguments,
-# finds the shortest path of internal hyperlinks between them.
+# finds the shortest path of internal hyperlinks between them
+# using iterated deepening.
 
 import sys, urllib2
 from bs4 import BeautifulSoup
@@ -14,9 +15,8 @@ end = sys.argv[2] # target article
 # widipedia's articles' urls are all head + middle + article name
 head, middle = 'https://en.wikipedia.org', '/wiki/'
 
-# at any given point in the search, this list will hold the articles
-# in our path from the start to the current article.
-# it can be thought of as a trail of breadcrumbs.
+# at any given point in the search, path will hold the trail taken
+# to the current page
 path = []
 
 # during the searching towards a target depth, contains the pages
@@ -46,48 +46,56 @@ def getBranches(page):
              if middle in href and ':' not in href and '.org' not in href
            ]
 
-# an iterative deepening search that, given a page and distance from
-# the current target depth, checks for solution and either ends or backtracks
-# if this distance is zero, otherwise calls itself for each possible branch.
+# an iterative deepening search that, given a page, the distance from
+# the current target depth, and the trail so far, checks for solution
+# and either ends or backtracks if this distance is zero, otherwise
+# calls itself for each possible branch.
 # it also prints information about the search as it goes.
 def search(page, distance):
 
     global history
 
-    # if we're restarting at a new depth, say so and clear history
-    if not path:
-        print '\nTARGET DEPTH:', distance
-        history = []
     # print current page at correct depth
-    print '  ' * len(path), '>', page
+    offsetPrint(len(path), page)
 
     path.append(page)
     history.append(page)
 
-    # if we're not at our target depth...
+    # if we're not at our target depth, then deepen
     if distance:
         for branch in getBranches(page):
             if branch not in history:
                 search(branch, distance - 1)
 
-    # if we are at our destination...
+    # if we're at our target depth and we have a match, end
     elif page == end:
         print '\nSOLUTION:'
-        # prettyprint the path. see below.
+        # prettyprint the path
         printPath(0, path)
         exit(0)
 
-    # backtrack one step up tree and pick up last breadcrumb
+    # pick up breadcrumb
     path.pop()
-    # if we're back at the top, start again with a deeper target depth
-    if not path: search(start, distance + 1)
 
 # prettyprints the path (so that it looks like the progress printing above)
 # increases indent with depth of step
 def printPath(depth, remainingPath):
     if remainingPath:
-        print ' ' * depth, '>', remainingPath[0]
+        offsetPrint(depth, remainingPath[0])
         printPath(depth + 1, remainingPath[1:])
 
+
+# prints a string at specified offset
+def offsetPrint(offset, value):
+    print '  ' * offset, '>', value
+
+
 if __name__ == '__main__':
-    search(start, 0)
+    depth = 0
+    # repeatedly deepen search
+    while True:
+        # when restarting at a new depth, say so and clear history
+        print '\nTARGET DEPTH:', depth
+        history = []
+        search(start, depth)
+        depth += 1
