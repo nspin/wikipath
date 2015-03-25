d160bf520d5b8a1d4c34acc6f93f312a53cd10a3
Nick Spinale <spinalen@carleton.edu>
2015-03-25 04:15:53 -0500

    more readable

--- WikiPath.hs
+++ WikiPath.hs
@@ -8,14 +8,20 @@ import           Control.Lens hiding (elements)
 import           Control.Monad
 import qualified Data.ByteString.Lazy as B
 import           Data.ByteString.Lens
+import           Data.Foldable hiding (elem)
 import           Data.List
--- import qualified Data.Text as T
 import           Data.Text.Lens
 import           Data.Maybe
 import qualified Data.Map as M
+import           Data.Monoid
 import           Network.Wreq
 import           System.Environment
 import           Text.Taggy.Lens
+
+-- Why doesn't this already exist (for all alternatives)
+instance Monoid (Concurrently a) where
+    mempty = empty
+    mappend = (<|>)
 
 sleep = forever $ threadDelay maxBound
 
@@ -28,19 +34,22 @@ main = do
                 let new = M.insert curr prev h
                 writeTVar hist new
                 return $ if curr == end
-                         then print
-                            . (start :)
-                            . reverse
-                            . takeWhile (/= start)
-                            $ iterate (new M.!) end
-                         else get ("http://www.wikipedia.com/wiki/" ++ curr) >>=
-                               ( runConcurrently
-                               . foldr (<|>) empty
-                               . map (Concurrently . go curr)
-                               . links
-                               )
+                         then print . intercalate " --> " $ gather start end new
+                         else kids curr >>= choices (go curr)
     go undefined start
 
+-- THIS IS SO LENS-LIKE
+choices :: (a -> IO b) -> [a] -> IO b
+choices f = runConcurrently . foldMap (Concurrently . f)
+
+gather :: String -> String -> M.Map String String -> [String]
+gather start end map = (start :)
+                     . reverse
+                     . takeWhile (/= start)
+                     $ iterate (map M.!) end
+
+kids :: String -> IO [String]
+kids = fmap links . get . ("http://www.wikipedia.com/wiki/" ++)
 
 links :: Response B.ByteString -> [String]
 links = map (takeWhile (/= '#'))
